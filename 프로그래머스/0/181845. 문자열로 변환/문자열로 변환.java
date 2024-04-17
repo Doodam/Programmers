027a9c2f9a16509dbcdd3bb2f08ca37ff84f0b69
Doooodam <121950964+Doodam@users.noreply.github.com>
2024-04-17 18:41:52 +0900

    [level 0] Title: 문자열로 변환, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        int number = n;
+        String answer = Integer.toString(number);
+        
+        
+        return answer;
+    }
+}
