54a176691c88cdf8750744cb1e55000dc726bc08
Doooodam <121950964+Doodam@users.noreply.github.com>
2024-04-23 19:06:12 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,25 +1,17 @@
 class Solution {
     public int solution(int[] num_list) {
         int multiplication = 1;
-        int sumSquare = 0;
         int sum = 0;
+        int answer = 0;
         
         for(int i=0; i<num_list.length; i++) {
             multiplication *= num_list[i]; 
+            sum += num_list[i];
         }
-        for (int m=0; m<num_list.length; m++) {
-            sum += num_list[m];
-            sumSquare = (int) Math.pow(sum, 2);
-        } 
         
-         
-        if(multiplication < sumSquare) {
-           return 1;
-         } else if(multiplication > sumSquare) {
-           return 0;
-        } else {
-            return -1;
-        }
+        answer = (int) Math.pow(sum, 2) > multiplication ? 1 : 0;
+        
+        return answer;
         
     }
 }
